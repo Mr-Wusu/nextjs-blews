97a25f161d4972d59e22e18aaba94da00c9b9471
Mr-Wusu <prince.agboinou_wusu@yahoo.com>
2025-01-04 23:39:59 +0100

    styled the provider buttons

--- app/_components/LoginForm.tsx
+++ app/_components/LoginForm.tsx
@@ -1,10 +1,13 @@
-import { loginWithProvider } from "../actions"
+import { loginWithProvider } from "../actions";
 
 function LoginForm() {
   return (
-    <form className="h-min py-4 px-5 rounded-md bg-rose-700" action={loginWithProvider}>
+    <form
+      className="h-min flex flex-col gap-4 py-4 px-5 rounded-md"
+      action={loginWithProvider}
+    >
       <button
-        className="bg-slate-400 text-[#fff] rounded-md py-2 px-2"
+        className="bg-slate-400 hover:bg-[#b3c1d4] text-[#fff] rounded-[.5rem] py-2 px-2 transition-all duration-300"
         type="submit"
         name="action"
         value="google"
@@ -13,7 +16,7 @@ function LoginForm() {
       </button>
 
       <button
-        className="bg-[#8644f0] text-white rounded-md py-2 px-2"
+        className="bg-[#8644f0] hover:bg-[#ad86ed] rounded-[.5rem] text-white py-2 px-2 transition-all duration-300"
         type="submit"
         name="action"
         value="facebook"
@@ -24,4 +27,4 @@ function LoginForm() {
   );
 }
 
-export default LoginForm
+export default LoginForm;
--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "@/app/_components/LoginForm";
 export default function SignIn() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="bg-rose-700 ">
+      <div className="">
         <LoginForm />
       </div>
     </div>
