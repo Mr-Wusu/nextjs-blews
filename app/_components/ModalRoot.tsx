c79dba45262cb5d373b5c6bf16d6af15d6dc89ff
Mr-Wusu <prince.agboinou_wusu@yahoo.com>
2025-05-16 13:54:56 +0100

    Did some homecleaning in preparation for deployment

--- app/_components/ModalRoot.tsx
+++ app/_components/ModalRoot.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createPortal } from "react-dom";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { IoClose } from "react-icons/io5";
 
@@ -13,18 +13,26 @@ export default function ModalRoot({
   className?: string;
 }>) {
   const modalRef = useRef<HTMLDialogElement>(null);
-  const modalUpload = document.getElementById("root-modal");
+  const [modalUpload, setModalUpload] = useState<HTMLElement | null>(null);
   const router = useRouter();
 
-  function handleCloseModal() {
-    router.back();
-  }
-
   useEffect(() => {
-    if (!modalRef.current?.open) {
-      modalRef.current?.showModal();
+    // Access document only in the browser
+    const rootModal = document.getElementById("root-modal");
+    setModalUpload(rootModal);
+
+    // Show the modal if not already open
+    if (rootModal && modalRef.current && !modalRef.current.open) {
+      modalRef.current.showModal();
     }
   }, []);
+
+  function handleCloseModal() {
+    if (modalRef.current) {
+      modalRef.current.close();
+    }
+    router.back();
+  }
 
   if (!modalUpload) return null;
 
@@ -34,13 +42,12 @@ export default function ModalRoot({
       ref={modalRef}
       onClose={handleCloseModal}
     >
-      <div className=" bg-rose-600 hover:bg-rose-500 transition-all duration-300 h-7 w-7 flex items-center justify-center  absolute top-0 right-0 rounded-tr-[.7rem] rounded-bl-[.7rem] z-40">
+      <div className="bg-rose-600 hover:bg-rose-500 transition-all duration-300 h-7 w-7 flex items-center justify-center absolute top-0 right-0 rounded-tr-[.7rem] rounded-bl-[.7rem] z-40">
         <IoClose
           className="cursor-pointer text-2xl text-lightRose1"
           onClick={handleCloseModal}
         />
       </div>
-
       {children}
     </dialog>,
     modalUpload
