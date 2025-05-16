7dfd118eccb03050cea64befe8ff5d44772b9dbb
Mr-Wusu <prince.agboinou_wusu@yahoo.com>
2025-05-16 12:59:08 +0100

    Added fallback clothing array, incase of third-party mischief

--- data/clothings.ts
+++ data/clothings.ts
@@ -49,4 +49,76 @@ const clothes = [
   },
 ];
 
+export const clothesDataConvex = [
+  {
+    alt: "To be deleted",
+    description: "Hey there, just testing the delete functionality",
+    price: 12345,
+    image: "any random photo",
+  },
+  {
+    alt: "To A simple senator. ",
+    description:
+      "A simple senator. Neatly sewn. The perfect cloth to show humility while maintaining class.",
+    price: 80000,
+    image: "The green senator",
+  },
+  {
+    alt: "A very good-looking suit",
+    description:
+      "This right here is the groom's suit. Very classy and intentional. Let her be sure she made the right choice",
+    price: 87000,
+    image: "Suit with pocket chain",
+  },
+  {
+    alt: "Jack Bauer kind of jacket",
+    description:
+      "This one is a solid jacket for office work or for the actor in an action film. It would do well in covering those guns.",
+    price: 12345,
+    image: "Jacket for covering guns",
+  },
+  {
+    alt: "A senior man",
+    description:
+      "Can pass as a cultist or a high-class jaiye jaiye bro. Bold and fine-looking. The babes would love you for it!",
+    price: 78000,
+    image: "That guy with beret",
+  },
+  {
+    alt: "A complete suit.",
+    description:
+      "A complete suit with matching socks and shoes. Get that business deal signed in no time with this impressively bold suit.",
+    price: 120000,
+    image: "any random photo",
+  },
+  {
+    alt: "A man dressed classy",
+    description:
+      "Is it the shoes, glasses, jacket, jeans or even the dark top? Can be any of them, but they are perfect together",
+    price: 95000,
+    image: "Guy in Jeans leaning on block work",
+  },
+  {
+    alt: "An array of beautiful Yoruba caps",
+    description:
+      "Your varieties for groom's men fila. Different shades for different places, yet each screaming class.",
+    price: 10000,
+    image: "A set of caps",
+  },
+  {
+    alt: "Dashing outing short-sleeve",
+    description:
+      "A semi-formal short sleeve, perfect for fridays night out with the guys. Silky, tough yet light on your skin.",
+    price: 20000,
+    image: "Short-sleeve shirt",
+  },
+  {
+    alt: "A Nigerian northern style agbada",
+    description:
+      "Sky blue northern style agbada sewn from a silky and classy material. Wear it for your campaigns to get the votes",
+    price: 80000,
+    image: "Sky blue agbada",
+  },
+];
+
 export default clothes;
