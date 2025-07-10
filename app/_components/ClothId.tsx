f5ee58969c05a9dc8e0ad8ed929ccc25b0fcb60b
Mr-Wusu <prince.agboinou_wusu@yahoo.com>
2025-07-10 03:22:37 +0100

    added styles on clothId page

--- app/_components/ClothId.tsx
+++ app/_components/ClothId.tsx
@@ -25,15 +25,15 @@ export default function ClothId({
   }
 
   if (!cloth) {
-    return <div>Cloth not found</div>;
+    return <div>Cloth not found! Might have been deleted from database</div>;
   }
 
   return (
     <div
-      className={`flex bg-rose-100 w-full flex-col gap-4 pb-5 overflow-hidden mt-14 md:pb-6  ${bg}`}
+      className={`flex bg-rose-100 w-full max-w-[28rem] flex-col lg+:flex-row lg+:w-fit lg+:max-w-none lg+:mx-auto shadow-md shadow-black pb-5 overflow-hidden mt-14 lg+:mt-[5.5rem] md:pb-6 lg+:pb-0 lg+:mb-5 ${bg}`}
     >
       <div
-        className={`relative w-full h-[21rem]   md:h-[34rem] overflow-hidden ${clothContainer}`}
+        className={`relative w-full h-[21rem] lg+:w-[26rem] md:h-[28rem] lg+:h-[34rem] overflow-hidden ${clothContainer}`}
       >
         <Image
           fill
@@ -44,7 +44,7 @@ export default function ClothId({
         />
       </div>
       <div
-        className={`flex flex-col gap-3 px-5 md:px-10 md:pt-5 md:text-lg md:w-5/6 mx-auto ${className}`}
+        className={`flex flex-col gap-3 px-5 md:px-10 md:pt-5 md:text-lg md:w-5/6 mx-auto lg+:w-[25rem] lg+:items-center lg+:justify-center ${className}`}
       >
         <h1>Cloth Id: {cloth._id}</h1>
         <div className="flex flex-col mt-2 md:text-[1.1rem]">
@@ -54,8 +54,19 @@ export default function ClothId({
             {`${cloth.description} ipsum dolor sit amet, consectetur adipiscing elit. Ut ac porttitor metus, et consequat eros.`}
           </p>
         </div>
+        <div className="hidden gap-7 w-4/5 mx-auto items-center py-4 lg+:flex">
+          <button
+            className="border-2 h-fit leading-none border-rose-500 rounded-[.3rem] px-3 py-[6px] flex hover:bg-gradient-to-l hover:from-rose-400 hover:to-rose-100 transition-all duration-300 ease-linear tracking-wide"
+            onClick={handleClick}
+          >
+            &larr; Back
+          </button>
+          <Button className="tracking-wider px-2 w-fit" onClick={handleClick}>
+            Add to Cart
+          </Button>
+        </div>
       </div>
-      <div className="flex gap-7 w-4/5 ml-auto md:ml-[6.5rem] md+:ml-[7.4rem] items-center py-4">
+      <div className="flex gap-7 w-4/5 ml-auto md:ml-[6.5rem] md+:ml-[7.4rem] items-center py-4 lg+:hidden">
         <button
           className="border-2 h-fit leading-none border-rose-500 rounded-[.3rem] px-3 py-[6px] flex hover:bg-gradient-to-l hover:from-rose-400 hover:to-rose-100 transition-all duration-300 ease-linear tracking-wide"
           onClick={handleClick}
